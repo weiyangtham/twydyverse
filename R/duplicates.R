45c4c3f338a8ae79ac71ad2107df53e0609e9fd7
weiyangtham <weiyang.tham@gmail.com>
2018-03-29 23:45:28 -0400

    Convenience functions to filter duplicates/unique observations

--- R/duplicates.R
+++ R/duplicates.R
@@ -8,13 +8,36 @@
 #'
 #' @examples
 #'
-#' df = data.frame(grp1 = c())
+#' # df = data.frame(grp1 = c())
 
 filter_duplicates <- function(data, ...) {
-  group_var <- quos(...)
+  group_var <- dplyr::quos(...)
 
   data %>%
-    group_by(!!! group_var) %>%
-    filter(max(row_number()) > 1) %>%
-    ungroup()
+    dplyr::group_by(!!! group_var) %>%
+    dplyr::filter(max(dplyr::row_number()) > 1) %>%
+    dplyr::ungroup()
 }
+
+#' Filter unique observations
+#'
+#' @param data
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+filter_unique <- function(data, ...) {
+  group_var <- dplyr::quos(...)
+
+  data %>%
+    dplyr::group_by(!!! group_var) %>%
+    dplyr::filter(max(dplyr::row_number()) == 1) %>%
+    dplyr::ungroup()
+}
+
+
+
+
