f802615a04c9144ddf53599b409cddd7b277f9f2
weiyangtham <weiyang.tham@gmail.com>
2018-03-31 00:34:50 -0400

    rdname

--- R/duplicates.R
+++ R/duplicates.R
@@ -1,14 +1,21 @@
 #' Filter duplicates on desired variables
 #'
+#' @description Filter duplicates or unique values. These are
+#' wrappers around a `group_by` then `filter`.
+#'
 #' @param data a tbl
 #' @param ... Variables to find duplicates of
 #'
-#' @return
 #' @export
 #'
 #' @examples
 #'
-#' # df = data.frame(grp1 = c())
+#' df <- data.frame(grp1 = c(1, 1, 2, 2, 3), grp2 = c(1, 2, 1, 2, 2))
+#'
+#' df %>% filter_duplicates(grp1)
+#' df %>% filter_duplicates(grp2)
+#'
+#' df %>% filter_unique(grp1)
 
 filter_duplicates <- function(data, ...) {
   group_var <- dplyr::quos(...)
@@ -19,22 +26,13 @@ filter_duplicates <- function(data, ...) {
     dplyr::ungroup()
 }
 
-#' Filter unique observations
-#'
-#' @param data
-#' @param ...
-#'
-#' @return
-#' @export
-#'
-#' @examples
-
+#' @rdname filter_duplicates
 filter_unique <- function(data, ...) {
   group_var <- dplyr::quos(...)
 
   data %>%
     dplyr::group_by(!!! group_var) %>%
-    dplyr::filter(max(dplyr::row_number()) == 1) %>%
+    dplyr::filter(max(row_number()) == 1) %>%
     dplyr::ungroup()
 }
 
